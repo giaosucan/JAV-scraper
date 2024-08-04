1d1a1ddfaa9bcdf5ff23c96b36b413d7e68b900f
trong.n <trong.n@scopicsoftware.com>
2024-08-04 09:09:12 +0000

    restrucre

--- README.md
+++ README.md
@@ -1,1 +1,4 @@
 # jav-scraper
+This script scrap the data from JAV website, download the poster and rename the folder to movie title
+
+# Prep
--- main.py
+++ main.py
@@ -1,55 +1,97 @@
-# import cloudscraper
 import json
 import os
 import time
 import logging
 import urllib.request
+import argparse
 from javlibrary import JAVLibrary
 
 # Configure logging
 logging.basicConfig(level=logging.DEBUG, format='%(asctime)s - %(levelname)s - %(message)s')
 
+def process_movie(directory_path, userAgent, javlibraryCFClearance):
+    """
+    Process each movie directory in the given path.
+
+    Args:
+        directory_path (str): The path to the directory containing movie directories.
+        userAgent (str): The user agent string to use for web requests.
+        javlibraryCFClearance (str): The CF clearance token to use for JAVLibrary requests.
+    """
+    
+    movie_dirs = get_list_folder(directory_path)
+    for movie_dir in movie_dirs:    
+        logging.debug(f"Processing movie directory: {movie_dir}") 
+        movie_info = get_movie_info(movie_dir, userAgent, javlibraryCFClearance)
+        if movie_info:
+            poster_urls = movie_info.get('art', [])           
+            if poster_urls:
+                download_poster(poster_urls, os.path.join(directory_path, movie_dir), movie_dir)
+                os.rename(directory_path + movie_dir, directory_path + movie_info['title'])
+            else:
+                logging.warning(f"No poster URLs found for movie code: {movie_dir}")
+        
+        time.sleep(3) ## Avoid to many request exception
+
 def get_list_folder(directory_path):
+    """
+    Get a list of all directories in the given path.
+    Args:
+        directory_path (str): The path to the directory.
+    Returns:
+        list: A list of directory names.
+    """
     return [name for name in os.listdir(directory_path) if os.path.isdir(os.path.join(directory_path, name))]
 
 def get_movie_info(code, userAgent, javlibraryCFClearance):
-    jav = JAVLibrary(userAgent, javlibraryCFClearance)
-    return jav.get_metadata(code)
+    """
+    Get the metadata for a movie using the JAVLibrary API.
+    Args:
+        code (str): The movie code.
+        userAgent (str): The user agent string to use for web requests.
+        javlibraryCFClearance (str): The Cloud Flare clearance cookies to use for JAVLibrary requests.
+    Returns:
+        dict: A dictionary containing the movie metadata, or None if the metadata could not be retrieved.
+    """
+    try:
+        jav = JAVLibrary(userAgent, javlibraryCFClearance)
+        return jav.get_metadata(code)
+    except Exception as e:
+        logging.error(f"Error getting movie info for code {code}: {e}")
+        return None
 
-def download_poster(urls,path,poster):
-    # Send a GET request to the URL
-    for url in urls:
-        try:
-            urllib.request.urlretrieve(url, path + poster + ".jpg")        
-            logging.debug('Image successfully downloaded and saved!')
-        except Exception as e:
-            logging.error(f'Failed to retrieve image. {e}')
-
-BASE_PATH = "/mnt/k/Adult/Japan/Censored/Temp"
+def download_poster(urls, path, poster):
+        """
+        Download a movie poster from the given URLs and save it to the specified path.
+        Args:
+            urls (list): A list of URLs where the poster image can be downloaded.
+            path (str): The path to the directory where the poster should be saved.
+            poster (str): The filename for the poster image.
+        """
+        for url in urls:
+            try:
+                urllib.request.urlretrieve(url, os.path.join(path, f"{poster}.jpg"))
+                logging.debug(f'Image successfully downloaded and saved to {os.path.join(path, f"{poster}.jpg")}')
+            except Exception as e:
+                logging.error(f'Failed to retrieve image from {url}. Error: {e}')
 
 def main():
-    # Your main code logic here
-    print("Start scrapping data")    
+    parser = argparse.ArgumentParser(description="JAV scrapper",
+                                 formatter_class=argparse.ArgumentDefaultsHelpFormatter)
+    parser.add_argument('directory_path', type=str, help='Path of the folder to scrap')
+    args = parser.parse_args()
+    config = vars(args)    
+ 
+    logging.info(f"Start scrapping data from folder {config["directory_path"]}...")       
+    
     # Load JSON data from a file
     with open('cookies.json', 'r') as file:
         data = json.load(file)
     # Accessing values
     userAgent = data["userAgent"]
-    javlibraryCFClearance = data["javlibraryCFClearance"]
-    list_code = get_list_folder(BASE_PATH)
-    logging.debug(f"list folders name {list_code}")    
-    for code in list_code:
-        try:
-            movie_info = get_movie_info(code,userAgent=userAgent,javlibraryCFClearance=javlibraryCFClearance)       
-            download_poster(movie_info['art'], BASE_PATH+ code + "/", code)
-            if len(movie_info['title']) >= 254:
-                cut_string =  movie_info['title'][:254]
-                os.rename(BASE_PATH+ code, BASE_PATH + cut_string)
-            else:
-                os.rename(BASE_PATH+ code, BASE_PATH + movie_info['title'])
-            time.sleep(5)
-        except Exception as e:
-            logging.error(f"Exception happen {e}")     
+    javlibraryCFClearance = data["javlibraryCFClearance"]    
+     
+    process_movie(config["directory_path"], userAgent, javlibraryCFClearance)  
 
 if __name__ == "__main__":
     main()
